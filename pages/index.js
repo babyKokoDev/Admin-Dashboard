312514d06d6a573afd63dc76ec7b13b11fc9584e
Elijah Aremu <aremuelijah.ae@gmail.com>
2023-04-22 15:53:34 +0100

    project initialization

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Header from '@/components/Header'
+import Head from 'next/head'
+import Image from 'next/image'
+
+
+export default function Home() {
+  return (
+    <>
+    <Head>
+     <title>Admin Dashboard</title>
+    </Head>
+    <main className="bg-gray-100 min-h-screen">
+      <Header />
+    </main>
+    </>
+  )
+}
