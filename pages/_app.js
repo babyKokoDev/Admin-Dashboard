eef1c7aaa26b529e7882058b91a6b55008dd60aa
Elijah Aremu <aremuelijah.ae@gmail.com>
2023-04-25 09:18:59 +0100

    authentication in progress

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
 import Sidebar from '@/components/Sidebar'
 import '@/styles/globals.css'
+import { SessionProvider } from "next-auth/react"
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+    <SessionProvider session={session}>
     <Sidebar>
     <Component {...pageProps} />
     </Sidebar>
+    </SessionProvider>
     </>
   )
 }
